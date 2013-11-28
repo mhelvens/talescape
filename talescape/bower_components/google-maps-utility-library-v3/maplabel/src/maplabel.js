e339f7e41d56c9363325e5b152996d50d898db8b
Michiel Helvensteijn <mhelvens@gmail.com>
2013-11-28 13:50:29 +0100

    Improved scenario message box.

--- talescape/bower_components/google-maps-utility-library-v3/maplabel/src/maplabel.js
+++ talescape/bower_components/google-maps-utility-library-v3/maplabel/src/maplabel.js
@@ -30,16 +30,16 @@
  * @param {Object.<string, *>=} opt_options Optional properties to set.
  */
 function MapLabel(opt_options) {
-	this.set('fontFamily', 'sans-serif');
-	this.set('fontSize', 12);
-	this.set('fontColor', '#000000');
-	this.set('strokeWeight', 4);
-	this.set('strokeColor', '#ffffff');
-	this.set('align', 'center');
+  this.set('fontFamily', 'sans-serif');
+  this.set('fontSize', 12);
+  this.set('fontColor', '#000000');
+  this.set('strokeWeight', 4);
+  this.set('strokeColor', '#ffffff');
+  this.set('align', 'center');
 
-	this.set('zIndex', 1e3);
+  this.set('zIndex', 1e3);
 
-	this.setValues(opt_options);
+  this.setValues(opt_options);
 }
 MapLabel.prototype = new google.maps.OverlayView;
 
@@ -47,78 +47,78 @@ window['MapLabel'] = MapLabel;
 
 
 /** @inheritDoc */
-MapLabel.prototype.changed = function (prop) {
-	switch (prop) {
-		case 'fontFamily':
-		case 'fontSize':
-		case 'fontColor':
-		case 'strokeWeight':
-		case 'strokeColor':
-		case 'align':
-		case 'text':
-			return this.drawCanvas_();
-		case 'maxZoom':
-		case 'minZoom':
-		case 'position':
-			return this.draw();
-	}
+MapLabel.prototype.changed = function(prop) {
+  switch (prop) {
+    case 'fontFamily':
+    case 'fontSize':
+    case 'fontColor':
+    case 'strokeWeight':
+    case 'strokeColor':
+    case 'align':
+    case 'text':
+      return this.drawCanvas_();
+    case 'maxZoom':
+    case 'minZoom':
+    case 'position':
+      return this.draw();
+  }
 };
 
 /**
  * Draws the label to the canvas 2d context.
  * @private
  */
-MapLabel.prototype.drawCanvas_ = function () {
-	var canvas = this.canvas_;
-	if (!canvas) return;
+MapLabel.prototype.drawCanvas_ = function() {
+  var canvas = this.canvas_;
+  if (!canvas) return;
 
-	var style = canvas.style;
-	style.zIndex = /** @type number */(this.get('zIndex'));
+  var style = canvas.style;
+  style.zIndex = /** @type number */(this.get('zIndex'));
 
-	var ctx = canvas.getContext('2d');
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.strokeStyle = this.get('strokeColor');
-	ctx.fillStyle = this.get('fontColor');
-	ctx.font = this.get('fontSize') + 'px ' + this.get('fontFamily');
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = this.get('strokeColor');
+  ctx.fillStyle = this.get('fontColor');
+  ctx.font = this.get('fontSize') + 'px ' + this.get('fontFamily');
 
-	var strokeWeight = Number(this.get('strokeWeight'));
+  var strokeWeight = Number(this.get('strokeWeight'));
 
-	var text = this.get('text');
-	if (text) {
-		if (strokeWeight) {
-			ctx.lineWidth = strokeWeight;
-			ctx.strokeText(text, strokeWeight, strokeWeight);
-		}
+  var text = this.get('text');
+  if (text) {
+    if (strokeWeight) {
+      ctx.lineWidth = strokeWeight;
+      ctx.strokeText(text, strokeWeight, strokeWeight);
+    }
 
-		ctx.fillText(text, strokeWeight, strokeWeight);
+    ctx.fillText(text, strokeWeight, strokeWeight);
 
-		var textMeasure = ctx.measureText(text);
-		var textWidth = textMeasure.width + strokeWeight;
-		style.marginLeft = this.getMarginLeft_(textWidth) + 'px';
-		// Bring actual text top in line with desired latitude.
-		// Cheaper than calculating height of text.
-		style.marginTop = '-0.4em';
-	}
+    var textMeasure = ctx.measureText(text);
+    var textWidth = textMeasure.width + strokeWeight;
+    style.marginLeft = this.getMarginLeft_(textWidth) + 'px';
+    // Bring actual text top in line with desired latitude.
+    // Cheaper than calculating height of text.
+    style.marginTop = '-0.4em';
+  }
 };
 
 /**
  * @inheritDoc
  */
-MapLabel.prototype.onAdd = function () {
-	var canvas = this.canvas_ = document.createElement('canvas');
-	var style = canvas.style;
-	style.position = 'absolute';
+MapLabel.prototype.onAdd = function() {
+  var canvas = this.canvas_ = document.createElement('canvas');
+  var style = canvas.style;
+  style.position = 'absolute';
 
-	var ctx = canvas.getContext('2d');
-	ctx.lineJoin = 'round';
-	ctx.textBaseline = 'top';
+  var ctx = canvas.getContext('2d');
+  ctx.lineJoin = 'round';
+  ctx.textBaseline = 'top';
 
-	this.drawCanvas_();
+  this.drawCanvas_();
 
-	var panes = this.getPanes();
-	if (panes) {
-		panes.mapPane.appendChild(canvas);
-	}
+  var panes = this.getPanes();
+  if (panes) {
+    panes.mapPane.appendChild(canvas);
+  }
 };
 MapLabel.prototype['onAdd'] = MapLabel.prototype.onAdd;
 
@@ -128,44 +128,44 @@ MapLabel.prototype['onAdd'] = MapLabel.prototype.onAdd;
  * @param {number} textWidth  the width of the text, in pixels.
  * @return {number} the margin-left, in pixels.
  */
-MapLabel.prototype.getMarginLeft_ = function (textWidth) {
-	switch (this.get('align')) {
-		case 'left':
-			return 0;
-		case 'right':
-			return -textWidth;
-	}
-	return textWidth / -2;
+MapLabel.prototype.getMarginLeft_ = function(textWidth) {
+  switch (this.get('align')) {
+    case 'left':
+      return 0;
+    case 'right':
+      return -textWidth;
+  }
+  return textWidth / -2;
 };
 
 /**
  * @inheritDoc
  */
-MapLabel.prototype.draw = function () {
-	var projection = this.getProjection();
+MapLabel.prototype.draw = function() {
+  var projection = this.getProjection();
 
-	if (!projection) {
-		// The map projection is not ready yet so do nothing
-		return;
-	}
+  if (!projection) {
+    // The map projection is not ready yet so do nothing
+    return;
+  }
 
-	if (!this.canvas_) {
-		// onAdd has not been called yet.
-		return;
-	}
+  if (!this.canvas_) {
+    // onAdd has not been called yet.
+    return;
+  }
 
-	var latLng = /** @type {google.maps.LatLng} */ (this.get('position'));
-	if (!latLng) {
-		return;
-	}
-	var pos = projection.fromLatLngToDivPixel(latLng);
+  var latLng = /** @type {google.maps.LatLng} */ (this.get('position'));
+  if (!latLng) {
+    return;
+  }
+  var pos = projection.fromLatLngToDivPixel(latLng);
 
-	var style = this.canvas_.style;
+  var style = this.canvas_.style;
 
-	style['top'] = pos.y + 'px';
-	style['left'] = pos.x + 'px';
+  style['top'] = pos.y + 'px';
+  style['left'] = pos.x + 'px';
 
-	style['visibility'] = this.getVisible_();
+  style['visibility'] = this.getVisible_();
 };
 MapLabel.prototype['draw'] = MapLabel.prototype.draw;
 
@@ -174,33 +174,33 @@ MapLabel.prototype['draw'] = MapLabel.prototype.draw;
  * @private
  * @return {string} blank string if visible, 'hidden' if invisible.
  */
-MapLabel.prototype.getVisible_ = function () {
-	var minZoom = /** @type number */(this.get('minZoom'));
-	var maxZoom = /** @type number */(this.get('maxZoom'));
+MapLabel.prototype.getVisible_ = function() {
+  var minZoom = /** @type number */(this.get('minZoom'));
+  var maxZoom = /** @type number */(this.get('maxZoom'));
 
-	if (minZoom === undefined && maxZoom === undefined) {
-		return '';
-	}
+  if (minZoom === undefined && maxZoom === undefined) {
+    return '';
+  }
 
-	var map = this.getMap();
-	if (!map) {
-		return '';
-	}
+  var map = this.getMap();
+  if (!map) {
+    return '';
+  }
 
-	var mapZoom = map.getZoom();
-	if (mapZoom < minZoom || mapZoom > maxZoom) {
-		return 'hidden';
-	}
-	return '';
+  var mapZoom = map.getZoom();
+  if (mapZoom < minZoom || mapZoom > maxZoom) {
+    return 'hidden';
+  }
+  return '';
 };
 
 /**
  * @inheritDoc
  */
-MapLabel.prototype.onRemove = function () {
-	var canvas = this.canvas_;
-	if (canvas && canvas.parentNode) {
-		canvas.parentNode.removeChild(canvas);
-	}
+MapLabel.prototype.onRemove = function() {
+  var canvas = this.canvas_;
+  if (canvas && canvas.parentNode) {
+    canvas.parentNode.removeChild(canvas);
+  }
 };
 MapLabel.prototype['onRemove'] = MapLabel.prototype.onRemove;
